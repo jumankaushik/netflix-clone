3791da6e9d8f6d48786ace726fa7be5888cf395c
Juman Kaushik <juman.code@gmail.com>
2024-11-07 20:28:30 +0530

    initial commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './titleCard.css'
+
+const TitleCards = () => {
+  return (
+    <div className='titlecards'>TitleCards</div>
+  )
+}
+
+export default TitleCards
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './player.css'
+
+const Player = () => {
+  return (
+    <div className='player'>Player</div>
+  )
+}
+
+export default Player
